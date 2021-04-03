447125978d7f1941881296fa42a6d45c20180e5b
Shubham656jain <shubham656jain@gmail.com>
2021-04-03 14:02:20 +0530

    add changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
-const MongoDBURI = process.env.MONGO_URI || 'mongodb://localhost/ManualAuth';
+const MongoDBURI = 'mongodb+srv://shubham:QaK0SBLmnFqrfQSL@login-signup.htbxw.mongodb.net/ManualAuth';
 
 mongoose.connect(MongoDBURI, {
   useUnifiedTopology: true,
